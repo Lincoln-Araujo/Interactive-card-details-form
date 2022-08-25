afdada89da4bf83a26ac93f4663a84bd6545ea6b
Lincoln Araujo <lincoln.fa@hotmail.com>
2022-08-24 22:49:56 -0300

    updating div container

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
       <div className='container'>
         <img className="card" id="front-card" alt="" src={require('./images/bg-card-front.png')}/>
         <img className="card" id="back-card" alt="" src={require('./images/bg-card-back.png')}/>
-      </div>
-      
+      </div>      
     </div>
   );
 }
