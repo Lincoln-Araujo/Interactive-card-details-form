c86b98e2180f8f08d85f1945f4e3b25c0165188d
Lincoln Araujo <lincoln.fa@hotmail.com>
2022-08-26 01:13:10 -0300

    adding setState to input form

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import './Card.css';
 
 export default props =>  {
+
+    const [number, setNumber] = useState("0000 0000 0000 0000 ");
+
+    const [name, setName] = useState("YOUR NAME ");
+
+    const [month, setMonth] = useState("MM");
+
+    const [year, setYear] = useState("YY");
+
+    const [cvc, setCvc] = useState("000");
+
     return (
 
         <>
@@ -19,16 +30,16 @@ export default props =>  {
                     </div> 
 
                     <div className="card-number">
-                        <span className="text">0000 0000 0000 0000</span>
+                        <span className="text">{number}</span>
                     </div>   
 
                     <div className="card-info">
                         <div className="card-info-name">
-                            <span className="text small">Lincoln Araujo</span>
+                            <span className="text small">{name}</span>
                         </div>
 
                         <div className="card-info-date">
-                            <span className="text small">00/00</span>
+                            <span className="text small">{month}/{year}</span>
                         </div>
                     </div>
                 </div>
@@ -39,7 +50,7 @@ export default props =>  {
                     alt="back-card">
 
                     <div className="security-number">
-                        <span className="text small">000</span>
+                        <span className="text small">{cvc}</span>
                     </div>
 
                 </div>    
@@ -51,13 +62,13 @@ export default props =>  {
                     
                     <div className="form-control">
                         <h3 className="title">cardholder name</h3>
-                        <input type="text" placeholder="e.g. Lincoln Araujo" />
+                        <input type="text" onChange={e => setName(e.target.value)} placeholder="e.g. Lincoln Araujo" />
                     </div>
 
                     
                     <div className="form-control">
                         <h3 className="title">card number</h3>
-                        <input type="text" placeholder="1234 5678 9123 0000" />
+                        <input type="text" maxLength="19" onChange={e => setNumber(e.target.value)} placeholder="1234 5678 9123 0000" />
                     </div>
 
                     <div className="form-group">
@@ -65,20 +76,23 @@ export default props =>  {
                         <div className="form-control">
                             <h3 className="title">exp. date (mm/yy)</h3>
                             <div className="form-date">
-                                <input type="text" id='month' placeholder="MM" />
-                                <input type="text" id='year' placeholder="YY" />
+                                <input type="text" maxLength="2" onChange={e => setMonth(e.target.value)} id='month' placeholder="MM" />
+                                <input type="text" maxLength="2" onChange={e => setYear(e.target.value)} id='year' placeholder="YY" />
                             </div>                            
                         </div>
-
                         
                         <div className="form-control">
                             <h3 className="title">cvc</h3>
-                            <input type="text" placeholder="e.g. 123" />
+                            <input type="text" maxLength="3" onChange={e => setCvc(e.target.value)} placeholder="e.g. 123" />
                         </div>
+                    </div>                   
+
+                    <div className="btn text">
+                        Confirm
                     </div>
-                    
-                    
+
                 </div>
+
             </div>
         </>
         
